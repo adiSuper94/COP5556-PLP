3db4ed5577740cfc34a76fce59d43d54c3859b3e
Aditya Subramanian <aditya.subramani@ufl.edu>
2021-10-12 12:56:16 -0400

    Edit IPLPParser def
    assignment testcases fail.

--- src/main/java/edu/ufl/cise/plpfa21/assignment1/CompilerComponentFactory.java
+++ src/main/java/edu/ufl/cise/plpfa21/assignment1/CompilerComponentFactory.java
@@ -6,12 +6,12 @@ import edu.ufl.cise.plpfa21.assignment2.RecDecParser;
 public class CompilerComponentFactory {
 
     static IPLPLexer getLexer(String input) {
-        //TODO  create and return a Lexer instance to parse the given input.
         return new DFALexer(input);
     }
 
 
     public static IPLPParser getParser(String input) {
+        //TODO : Return new parser that returns AST when parse method is called.
         IPLPLexer lexer = getLexer(input);
         return new RecDecParser(lexer);
     }
